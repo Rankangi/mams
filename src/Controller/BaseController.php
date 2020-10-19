311eaac8b4f745f4965c0ef38304053bcc2963d0
Mathieu <Mathieu.bann@gmail.com>
2020-10-19 16:41:32 +0200

    ajout fixtures + catégories

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -3,6 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Entity\Category;
+use App\Repository\ArticleRepository;
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,25 +18,46 @@ class BaseController extends AbstractController
     public function index()
     {
 
-        $repo = $this->getDoctrine()->getRepository(Article::class);
+        $repo = $this->getDoctrine()->getRepository(Category::class);
 
-        $articles = $repo->findAll();
+        $categories = $repo->findAll();
 
         return $this->render('base/index.html.twig', [
             'controller_name' => 'BaseController',
-            'articles' => $articles
+            'categories' => $categories
         ]);
     }
 
     /**
-     * @Route("/{id}", name="article_show")
+     * @Route("/article/{id}", name="article_show")
      * @param Article $article
+     * @param ArticleRepository $repo
      * @return Response
      */
-    public function showArticle(Article $article)
+    public function showArticle(Article $article, ArticleRepository $repo)
     {
+
         return $this->render('base/article.html.twig', [
             'article' => $article
         ]);
     }
+
+    /**
+     * @Route("/category/{id}", name="category_show")
+     * @param Category $category
+     * @param CategoryRepository $repo
+     * @return Response
+     */
+    public function showCategory(Category $category, CategoryRepository $repo)
+    {
+
+        $categories = $repo->findAll();
+
+        return $this->render('base/category.html.twig', [
+            'categories' => $categories,
+            'category' => $category
+        ]);
+    }
+
+
 }
--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -3,23 +3,42 @@
 namespace App\DataFixtures;
 
 use App\Entity\Article;
+use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class ArticleFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
 
-        for($i = 1; $i <= 10; $i++){
-            $article = new Article();
-            $article->setTitle("Titre de l'article n°$i")
-                    ->setDescription("Contenu de l'article n°$i")
-                    ->setContent("Voici le contenu de l'article n°$i.\nNous sommes sur quelque chose de qualité!")
+        // Création de 3 à 5 Catégories
+        $faker = Factory::create('fr_FR');
+
+        for ($j = 1; $j <= mt_rand(3,5); $j++) {
+            $category = new Category();
+            $category->setTitle($faker->sentence())
+                ->setDescription($faker->paragraph());
+
+            $manager->persist($category);
+
+            // Création de 5 à 10 articles
+            for($i = 1; $i <= mt_rand(5,10); $i++){
+                $article = new Article();
+
+                $content = '<p>' . join($faker->paragraphs(3), '</p><p>') . '</p>';
+
+                $article->setTitle($faker->sentence(4))
+                    ->setDescription($faker->paragraph())
+                    ->setContent($content)
                     ->setImage("http://placehold.it/700x400")
-                    ->setAmount(10)
-                    ->setPrice(10);
-            $manager->persist($article);
+                    ->setAmount(mt_rand(0,10))
+                    ->setPrice($faker->randomFloat(2,5,30))
+                    ->setCategory($category);
+
+                $manager->persist($article);
+            }
         }
 
         $manager->flush();
