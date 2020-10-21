63a1fbbc50dd6a38d79bc014f8cf76cb1570eb35
Mathieu <Mathieu.bann@gmail.com>
2020-10-21 16:23:24 +0200

    Passage de la quantité avant checkout + création page succès

--- src/Controller/CheckoutController.php
+++ src/Controller/CheckoutController.php
@@ -7,41 +7,59 @@ use Stripe\Checkout\Session;
 use Stripe\Stripe;
 use Stripe\Exception\ApiErrorException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class CheckoutController extends AbstractController
 {
     /**
      * @Route("/checkout/{id}", name="checkout")
      * @param Article $article
+     * @param Request $request
      * @return Response
      * @throws ApiErrorException
      */
-    public function index(Article $article)
+    public function index(Article $article, Request $request)
     {
-        Stripe::setApiKey($this->getParameter('stripe_secret_key'));
+        $amount = $request->query->get('amount');
 
+        Stripe::setApiKey($this->getParameter('stripe_secret_key'));
         $checkout_session = Session::create([
             'payment_method_types' => ['card'],
             'line_items' => [[
+                'quantity' => $amount,
                 'price_data' => [
                     'currency' => 'eur',
-                    'unit_amount' => 2000,
+                    'unit_amount' => $article->getPrice()*100,
                     'product_data' => [
-                        'name' => "test",
-                        'images' => ["http://placehold.it/700x400"],
+                        'name' => $article->getTitle(),
+                        'images' => [$article->getImage()],
+                        'description' => $article->getDescription(),
                     ],
                 ],
-                'quantity' => 10,
             ]],
             'mode' => 'payment',
-            'success_url' => "https://example.com/success",
+            'success_url' => $this->generateUrl('checkout_success',['id' => $article->getId(), 'amount' => $amount],UrlGeneratorInterface::ABSOLUTE_URL),
             'cancel_url' => "https://example.com/cancel",
         ]);
         $response = new Response();
         $response->headers->set('id', $checkout_session->id);
         return $response;
     }
+
+    /**
+     * @Route("/success", name="checkout_success")
+     * @param Request $request
+     * @return Response
+     */
+    public function checkout_success(Request $request){
+        $id = $request->query->get('id');
+        $amount = $request->query->get('amount');
+        return $this->render('checkout/success.html.twig');
+    }
+
+
 
 }
